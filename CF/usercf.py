358ae84f61c433c8cbcfccb1b2ed67d61b066044
SlowDive <18438605721@163.com>
2022-03-21 20:05:28 +0800

    Update usercf.py

--- CF/usercf.py
+++ CF/usercf.py
@@ -1,1 +1,152 @@
+'''
+参考自https://tianchi.aliyun.com/notebook-ai/detail?spm=5176.12586969.1002.3.410521d8yGZf7j&postId=144452
+'''
+import time, math, os
+from tqdm import tqdm
+import gc
+import pickle
+import random
+from datetime import datetime
+from operator import itemgetter
+import numpy as np
+import pandas as pd
+import warnings
+from collections import defaultdict
+import collections
+warnings.filterwarnings('ignore')
 
+
+# 根据时间获取商品被点击的用户序列  {item1: [(user1, time1), (user2, time2)...]...}
+# 这里的时间是用户点击当前商品的时间，好像没有直接的关系。
+def get_item_user_time_dict(click_df):
+    def make_user_time_pair(df):
+        return list(zip(df['user_id'], df['click_timestamp']))
+    
+    click_df = click_df.sort_values('click_timestamp')
+    item_user_time_df = click_df.groupby('click_article_id')['user_id', 'click_timestamp'].apply(lambda x: make_user_time_pair(x))\
+                                                            .reset_index().rename(columns={0: 'user_time_list'})
+    
+    item_user_time_dict = dict(zip(item_user_time_df['click_article_id'], item_user_time_df['user_time_list']))
+    return item_user_time_dict
+  
+  
+# 获取点击最多的文章
+def get_item_topk_click(click_df, k):
+    topk_click = click_df['click_article_id'].value_counts().index[:k]
+    return topk_click
+  
+  
+# 在计算用户之间的相似度的时候，也可以使用一些简单的关联规则，比如用户活跃度权重，这里将用户的点击次数作为用户活跃度的指标
+def get_user_activate_degree_dict(all_click_df):
+    all_click_df_ = all_click_df.groupby('user_id')['click_article_id'].count().reset_index()
+    
+    # 用户活跃度归一化
+    mm = MinMaxScaler()
+    all_click_df_['click_article_id'] = mm.fit_transform(all_click_df_[['click_article_id']])
+    user_activate_degree_dict = dict(zip(all_click_df_['user_id'], all_click_df_['click_article_id']))
+    return user_activate_degree_dict
+
+
+def usercf_sim(all_click_df, user_activate_degree_dict):
+    """
+        用户相似性矩阵计算
+        :param all_click_df: 数据表
+        :param user_activate_degree_dict: 用户活跃度的字典
+        return 用户相似性矩阵
+        
+        思路: 基于用户的协同过滤+ 关联规则
+    """
+    item_user_time_dict = get_item_user_time_dict(all_click_df)
+    u2u_sim = {}
+    user_cnt = defaultdict(int)
+    for item, user_time_list in tqdm(item_user_time_dict.items()):
+        for u, click_time in user_time_list:
+            user_cnt[u] += 1
+            u2u_sim.setdefault(u, {})
+            for v, click_time in user_time_list:
+                u2u_sim[u].setdefault(v, 0)
+                if u == v:
+                    continue
+                # 用户平均活跃度作为活跃度的权重，这里的式子也可以改善
+                activate_weight = 100 * 0.5 * (user_activate_degree_dict[u] + user_activate_degree_dict[v])   
+                u2u_sim[u][v] += activate_weight / math.log(len(user_time_list) + 1)
+    
+    u2u_sim_ = u2u_sim.copy()
+    for u, related_users in u2u_sim.items():
+        for v, wij in related_users.items():
+            u2u_sim_[u][v] = wij / math.sqrt(user_cnt[u] * user_cnt[v])
+    
+    # 将得到的相似性矩阵保存到本地
+    pickle.dump(u2u_sim_, open(save_path + 'usercf_u2u_sim.pkl', 'wb'))
+    return u2u_sim_
+  
+  
+# 基于用户的召回 u2u2i
+def user_based_recommend(user_id, user_item_time_dict, u2u_sim, sim_user_topk, recall_item_num, 
+                         item_topk_click, item_created_time_dict, emb_i2i_sim):
+    """
+        基于文章协同过滤的召回
+        :param user_id: 用户id
+        :param user_item_time_dict: 字典, 根据点击时间获取用户的点击文章序列   {user1: [(item1, time1), (item2, time2)..]...}
+        :param u2u_sim: 字典，文章相似性矩阵
+        :param sim_user_topk: 整数， 选择与当前用户最相似的前k个用户
+        :param recall_item_num: 整数， 最后的召回文章数量
+        :param item_topk_click: 列表，点击次数最多的文章列表，用户召回补全
+        :param item_created_time_dict: 文章创建时间列表
+        :param emb_i2i_sim: 字典基于内容embedding算的文章相似矩阵
+        
+        return: 召回的文章列表 [(item1, score1), (item2, score2)...]
+    """
+    # 历史交互
+    user_item_time_list = user_item_time_dict[user_id]    #  [(item1, time1), (item2, time2)..]
+    user_hist_items = set([i for i, t in user_item_time_list])   # 存在一个用户与某篇文章的多次交互， 这里得去重
+    items_rank = {}
+    for sim_u, wuv in sorted(u2u_sim[user_id].items(), key=lambda x: x[1], reverse=True)[:sim_user_topk]:
+        for i, click_time in user_item_time_dict[sim_u]:
+            if i in user_hist_items:
+                continue
+            items_rank.setdefault(i, 0)
+            
+            loc_weight = 1.0
+            content_weight = 1.0
+            created_time_weight = 1.0
+            
+            # 当前文章与该用户看的历史文章进行一个权重交互
+            for loc, (j, click_time) in enumerate(user_item_time_list):
+                # 点击时的相对位置权重
+                loc_weight += 0.9 ** (len(user_item_time_list) - loc)
+                # 内容相似性权重
+                if emb_i2i_sim.get(i, {}).get(j, None) is not None:
+                    content_weight += emb_i2i_sim[i][j]
+                if emb_i2i_sim.get(j, {}).get(i, None) is not None:
+                    content_weight += emb_i2i_sim[j][i]
+                
+                # 创建时间差权重
+                created_time_weight += np.exp(0.8 * np.abs(item_created_time_dict[i] - item_created_time_dict[j]))
+                
+            items_rank[i] += loc_weight * content_weight * created_time_weight * wuv
+        
+    # 热度补全
+    if len(items_rank) < recall_item_num:
+        for i, item in enumerate(item_topk_click):
+            if item in items_rank.items(): # 填充的item应该不在原来的列表中
+                continue
+            items_rank[item] = - i - 100 # 随便给个复数就行
+            if len(items_rank) == recall_item_num:
+                break
+        
+    items_rank = sorted(items_rank.items(), key=lambda x: x[1], reverse=True)[:recall_item_num]    
+    return items_rank
+  
+# usercf 召回
+u2u_sim = pickle.load(open(save_path + 'usercf_u2u_sim.pkl', 'rb'))
+
+sim_user_topk = 20
+recall_item_num = 10
+item_topk_click = get_item_topk_click(trn_hist_click_df, k=50)
+
+for user in tqdm(trn_hist_click_df['user_id'].unique()):
+    user_recall_items_dict[user] = user_based_recommend(user, user_item_time_dict, u2u_sim, sim_user_topk, \
+                                                        recall_item_num, item_topk_click, item_created_time_dict, emb_i2i_sim)    
+
+pickle.dump(user_recall_items_dict, open(save_path + 'usercf_u2u2i_recall.pkl', 'wb'))
